ba9a54272ec930879ec2ab8f9dde80367cd383cf
Alex Hamenko <hamenko.aleksandr@gmail.com>
2022-12-29 19:25:26 +0200

    Add parse method

--- src/UserAgentParserInterface.php
+++ src/UserAgentParserInterface.php
@@ -4,6 +4,7 @@ namespace Hillel\UserAgent\ParserInterface;
 
 interface UserAgentParserInterface
 {
+    public function parse(): void;
     public function getBrowser(): string;
     public function getOS(): string;
 }
